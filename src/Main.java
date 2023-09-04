e7fc8a7d4d98e1285eba1317201c99212d0886d1
AndreyMukhataev <mukhataevaa@gmail.com>
2023-09-04 17:46:32 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Phone ph1 = new Phone();
+        Phone ph2 = new Phone("+79272796333", "Samsung") ;
+        Phone ph3 = new Phone("89277894554","Nokia", 123);
+
+        System.out.println("number: " + ph1.number + " model: " + ph1.model + " weight: "+ph1.weight );
+        System.out.println("number: " + ph2.number + " model: " + ph2.model + " weight: "+ph2.weight );
+        System.out.println("number: " + ph3.number + " model: " + ph3.model + " weight: "+ph3.weight );
+
+        ph1.receiveCall("Andrey") ;
+        ph2.receiveCall("Alex") ;
+        ph3.receiveCall("Jon") ;
+
+        System.out.println(ph1.getNumber() );
+        System.out.println(ph2.getNumber() );
+        System.out.println(ph3.getNumber() );
+
     }
 }
--- src/Phone.java
+++ src/Phone.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Phone {
+public class Phone {
+    public String number;
+    public String model;
+    public int weight;
+    public Phone() {
+        number = "Пусто";
+        model = "Пусто";
+    }
+
+    public Phone(String number, String model, int weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+
+    public void receiveCall(String name ){
+        System.out.println("Звонит " + name);
+    }
+    public String getNumber(){
+        return number;
+    }
+
+
 }
